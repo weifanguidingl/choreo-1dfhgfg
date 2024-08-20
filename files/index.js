9926cb04774e26334b7765861c69100e2e6ec9b3
weifanguidingl <97165931+weifanguidingl@users.noreply.github.com>
2024-08-20 00:35:49 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 3000;        
-const UUID = process.env.UUID || '0484069e-e8c4-4444-8888-93cd60cb716e'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || '4bc0496e-ef85-4b04-a99f-13dc70b92a31'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.abc.ac';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'abc.ccc.comg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'ey……NeCJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'cho.vgfeufl.eu.org';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZjIzMWQ1ZmVlYmUzZmZlZmQ4ODM4N2U1YzdlNjI5NTQiLCJ0IjoiNDAyNDUzMDYtMjcyYy00YmRkLThiMTctZDM4NWRiYTdiYmYxIiwicyI6Ik1EUTVNVGt5TURrdE1HWTFZeTAwT1RWaUxXRTNPV1F0TVdKaVpEWmpNakptWWpNdyJ9';
 const CFIP = process.env.CFIP || 'government.se';
 const NAME = process.env.NAME || 'Choreo';
 
